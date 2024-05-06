fc036bc7029a696f02dd2918afe2afe1f56c131b
Artem Haliv <artemiko.galiv@gmail.com>
2024-05-06 17:37:03 +0200

    First commit

---
+++ BlackJackGame/Form2.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace BlackJackGame
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 22.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(459, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 74);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BlackJack!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 22.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(78, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(328, 74);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Your count:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Franklin Gothic Medium", 22.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(734, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(386, 74);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enemy count:";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Franklin Gothic Medium", 22.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(747, 1159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(339, 78);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Pass";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(370, 403);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1050, 724);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Franklin Gothic Medium", 22.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(91, 1159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(339, 78);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Hit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::BlackJackGame.Properties.Resources._360_F_95834587_VGPHMDccEXKw6yTlVGuZeIl1WyhMAYLW;
+            this.ClientSize = new System.Drawing.Size(1239, 1322);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ BlackJackGame/Form2.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlackJackGame
+{
+    public partial class Form2 : Form
+    {
+        Random rnd = new Random();
+        public int my_counter = 0;
+        public int enemy_counter = 0;
+        public int card;
+        public int seccard;
+        public Form2()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            this.Height += 145;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Text = "Hit";
+            button2.Enabled = true;
+            button3.Enabled = true;
+            if (my_counter <= 21)
+            {
+                int firstCard = rnd.Next(2, 12);
+                my_counter += firstCard;
+                Cards(firstCard);
+                label2.Text = "Your count:" + my_counter;
+
+            }
+            else if (my_counter >= 22)
+            {
+                button3.Enabled = false;
+                MessageBox.Show("You've already had more than 21.");
+
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button3.Enabled = false;
+            int hit = rnd.Next(5, 5);
+            for (int i = 0; i < hit; i++)
+            {
+                int secondcard = rnd.Next(2, 12);
+                seccard += secondcard;
+                Cards(secondcard);
+                label3.Text = "Enemy count:" + seccard;
+                await Task.Delay(1500);
+                if (seccard >= 17 && seccard <= 21)
+                {
+                    break;
+                }
+                if (seccard > 21)
+                {
+                    break;
+                }
+
+
+            }
+
+            if (seccard > my_counter && seccard < 22)
+            {
+                LoseMessage();
+            }
+            else if (seccard == my_counter)
+            {
+                DialogResult result = MessageBox.Show("It's tie!", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    BackMenu();
+                }
+            }
+            else if (seccard < my_counter && my_counter < 22)
+            {
+                WinMessage();
+            }
+            else if (seccard > 21 && my_counter > 21 && seccard > my_counter)
+            {
+                WinMessage();
+            }
+            else if (seccard > 21 && my_counter > 21 && seccard < my_counter)
+            {
+                LoseMessage();
+            }
+            else if (seccard > 21 && my_counter < 22)
+            {
+                WinMessage();
+            }
+            else if (seccard < 22 && my_counter > 21)
+            {
+                LoseMessage();
+            }
+        }
+        private void WinMessage()
+        {
+            DialogResult result = MessageBox.Show("You win!", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (result == DialogResult.OK)
+            {
+                BackMenu();
+            }
+        }
+        private void LoseMessage()
+        {
+            DialogResult result = MessageBox.Show("You lose!", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (result == DialogResult.OK)
+            {
+                BackMenu();
+            }
+        }
+    
+        private void Cards(int num)
+        {
+            switch (num)
+            {
+                case 2:
+                    pictureBox1.Image = Properties.Resources.two;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.three;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.four;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources.six;
+                    break;
+                case 7:
+                    pictureBox1.Image = Properties.Resources.seven;
+                    break;
+                case 8:
+                    pictureBox1.Image = Properties.Resources.eight;
+                    break;
+                case 9:
+                    pictureBox1.Image = Properties.Resources.nine;
+                    break;
+                case 10:
+                    pictureBox1.Image = Properties.Resources.ten;
+                    break;
+                case 11:
+                    pictureBox1.Image = Properties.Resources.tuz;
+                    break;
+            }
+        }
+        public void BackMenu()
+        {
+            Form1 f2 = new Form1();
+            this.Hide();
+            f2.ShowDialog();
+            this.Close();
+        }
+    }
+}
